40975bfb6fec3f22cee8d6e5e8a318232de5f615
Leah Migacz <lmigacz@msudenver.edu>
2019-12-12 20:13:44 -0700

    finished functionality of the password

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var num = '0123456789';
+var sym = '!@#$%^&*=-_';
 
+var charNum = document.getElementById("charNum");
+var numBox = document.getElementById("num");
+var symBox = document.getElementById("sym");
+var submit = document.getElementById("submit");
+var yourPw = document.getElementById("yourPw");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+submit.addEventListener("click",function(e){
+    var characters = char;
+    (numBox.checked) ? characters += num : '';
+    (symBox.checked) ? characters += sym : '';
+    yourPw.value = password(charNum.value, characters);
+});
 
-  passwordText.value = password;
+function password(l,characters){
+		var pwd = '';
+    for(var i = 0; i<l; i++){
+    		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
 }
-
-function copyToClipboard() {
-  // BONUS 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// BONUS EVENT LISTENER
